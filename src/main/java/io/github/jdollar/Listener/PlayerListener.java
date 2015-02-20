de8cc1212e9a86f7d9e9195c1a922f162eaa2ca0
Joel Dollarhide <jdollarhide@systemsinmotion.com>
2015-02-20 01:26:17 -0500

    Updating the listener to new code

--- src/main/java/io/github/jdollar/Listener/PlayerListener.java
+++ src/main/java/io/github/jdollar/Listener/PlayerListener.java
@@ -1,12 +1,15 @@
-package io.github.jdollar.Listener;
+package io.github.jdollar.listener;
 
 import io.github.jdollar.FreezeKill;
 import io.github.jdollar.Timer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
 
 import java.util.*;
 
@@ -21,13 +24,15 @@ public final class PlayerListener implements Listener {
     @EventHandler
     public void freeze(PlayerMoveEvent playerMoveEvent) {
         if (frozenPlayers.containsKey(playerMoveEvent.getPlayer().getUniqueId())) {
-            playerMoveEvent.setCancelled(true);
+            Vector newDirection = playerMoveEvent.getTo().getDirection();
+            playerMoveEvent.setTo(playerMoveEvent.getFrom());
+            playerMoveEvent.getTo().setDirection(newDirection);
         }
     }
 
     @EventHandler
     public void killMob(EntityDeathEvent entityDeathEvent) {
-        if (entityDeathEvent.getEntity() instanceof Monster) {
+        if (entityDeathEvent.getEntity() instanceof Monster && entityDeathEvent.getEntity().getKiller() != null) {
             UUID monsterSlayer = entityDeathEvent.getEntity().getKiller().getUniqueId();
             if (frozenPlayers.containsKey(monsterSlayer)) {
                 //Player lives!
@@ -48,5 +53,9 @@ public final class PlayerListener implements Listener {
         if (frozenPlayers != null) {
             frozenPlayers.remove(playerUuid);
         }
+    }
+
+    public void setFrozenPlayers(Map<UUID, Timer> frozenPlayers) {
+        this.frozenPlayers = frozenPlayers;
     }
 }
